ec6aa9ac1ec289c7d71229f7eb7cc526a211cf03
Wilsont0554 <wilsont0554@my.uwstout.edu>
2025-02-12 22:33:31 -0600

    Fixed minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
 import InfoBox from "./components/InfoBox"
 import MetroMap from "./components/MetroMap"
+import { scryRenderedDOMComponentsWithTag } from "react-dom/test-utils";
 
 var currentLineID = 0; //represents the line_id who's information is being dispalyed
+var currentLinesStops = [];
+var displayedTripID = 0;
+var tracking = 0;
 
 function App() {
 
-  
   const [stationLines, setStationStops] = useState([]); //represents all line that stop at this station
   const [count, setCount] = useState(0);
   const [displayedPlaceCode, setPlaceCode] = useState([]); //represents the place code that the user has sleected
@@ -51,7 +54,7 @@ function App() {
   * Updates the fetch every 10 seconds while the infobox is displayed
   */
   useInterval(() => {
-    if (document.getElementById('hide').style.display == 'inline')
+    if (document.getElementById('hide').style.display == 'flex')
     {
       var direction0PlaceCode = displayedPlaceCode;
       var direction1PlaceCode = displayedPlaceCode;
@@ -70,7 +73,7 @@ function App() {
   * Used for making sure the infobox in above the station, updates every 10ms
   */
   useInterval(() => {
-    if (document.getElementById('hide').style.display == 'inline')
+    if (document.getElementById('hide').style.display == 'flex')
     {
       moveInfoBox(displayedPlaceCode);
     }
@@ -97,6 +100,10 @@ function App() {
     setStationStops(stationLines);
   }
 
+  function setCurrentLinesStops(stationStops){
+    currentLinesStops = stationStops;
+  }
+
   /*
   * Resets all line icon backgrouns
   * Then, sets the background color of the first, currently showing line information to the previsouly assigned color in the lineColors array
@@ -105,7 +112,9 @@ function App() {
     for (let i = 1; i <stationLines.length; i++){
       document.getElementById(stationLines[i]).style.setProperty("--background-color", 'rgb(234, 234, 234)');
     }    
-    document.getElementById(lineID).style.setProperty("--background-color", lineColors.get(lineID));
+    document.getElementById(lineID).style.setProperty("--background-color", lineColors.get(lineID));    
+    document.body.style.setProperty("--infoBx-background", lineColors.get(lineID));    
+
     setCount(1);
   }
 
@@ -122,7 +131,8 @@ function App() {
 
     //sets correct background color to the selected icon
     document.getElementById(lineID).style.setProperty("--background-color", lineColors.get(lineID));
-    
+    document.body.style.setProperty("--infoBx-background", lineColors.get(lineID));    
+
     setCount(1);
     displayInfoBox(lineID, moveBox, placeCode, placeCode);
   }
@@ -132,44 +142,51 @@ function App() {
     *   Then, Check for the alternate diretion
     *   Then disaply the text for the next train/bus going both direction
     */
-  async function displayInfoBox(lineID, moveBox, direction0PlaceCode, direction1PlaceCode ){
+  async function displayInfoBox(lineID, moveBox, direction0PlaceCode, direction1PlaceCode){
     currentLineID = lineID;
+    var currentStopBound = [];
+    var currentPlaceCode = direction0PlaceCode;
 
-    //gets data from one direction    
-    const currentStop0BoundFetch = await fetch('https://svc.metrotransit.org/nextrip/' + lineID + '/0/' + direction0PlaceCode);
-    var currentStop0Bound = await currentStop0BoundFetch.json();            
+    if (direction0PlaceCode != 0){
+      //gets data from one direction    
+      const currentStop0BoundFetch = await fetch('https://svc.metrotransit.org/nextrip/' + lineID + '/0/' + direction0PlaceCode);
+      var currentStop0Bound = await currentStop0BoundFetch.json();
+      var current0BoundDirection = currentStop0Bound.departures[0].direction_text;
+      document.getElementById('current0Direction').innerHTML = current0BoundDirection;
+      document.getElementById("placeCode0Arrival").innerHTML = (currentStop0Bound.departures[0].departure_text);
+      currentStopBound = currentStop0Bound;
+      displayedTripID = currentStopBound.departures[0].trip_id;
+    }
+    else if (direction0PlaceCode == 0){
+      displayedTripID = currentStopBound.departures[0].trip_id;
+      currentPlaceCode = direction1PlaceCode;
+    }
     
-    //gets data for the opposite direction
-    const currentStop1BoundFetch = await fetch('https://svc.metrotransit.org/nextrip/' + lineID + '/1/' + direction1PlaceCode);
-    var currentStop1Bound = await currentStop1BoundFetch.json();
-    
+    if (direction1PlaceCode != 0){
+      //gets data for the opposite direction
+      const currentStop1BoundFetch = await fetch('https://svc.metrotransit.org/nextrip/' + lineID + '/1/' + direction1PlaceCode);
+      var currentStop1Bound = await currentStop1BoundFetch.json();
+      var current1BoundDirection = currentStop1Bound.departures[0].direction_text;
+      document.getElementById('current1Direction').innerHTML = current1BoundDirection;
+      document.getElementById("placeCode1Arrival").innerHTML = (currentStop1Bound.departures[0].departure_text);
+      currentStopBound = currentStop1Bound;
+    }                
+       
     var infoBoxCoords = document.getElementById('hide');
 
-    var currentStopName = currentStop0Bound.stops[0].description;
-    var currentLineName = currentStop0Bound.departures[1].route_short_name;
-
-    var current0BoundDirection = currentStop0Bound.departures[0].direction_text;
-    var current1BoundDirection = currentStop1Bound.departures[0].direction_text;
-
-    //display on info to user
-    infoBoxCoords.style.display = 'inline';
+    var currentStopName = currentStopBound.stops[0].description;
+    var currentLineName = currentStopBound.departures[1].route_short_name;
     
     document.getElementById('stopName').innerHTML = currentStopName;
-    
     document.getElementById('currentLine').innerHTML = currentLineName;
-    
-    document.getElementById('current0Direction').innerHTML = current0BoundDirection;
-    document.getElementById("placeCode0Arrival").innerHTML = (currentStop0Bound.departures[0].departure_text);
-
-
-    document.getElementById('current1Direction').innerHTML = current1BoundDirection;
-    document.getElementById("placeCode1Arrival").innerHTML = (currentStop1Bound.departures[0].departure_text);
     
     if (moveBox)
     {
       disaplyInitialLineIcon(lineID);
-      moveInfoBox(direction0PlaceCode);
+      moveInfoBox(currentPlaceCode);
     }
+    //display on info to user
+    infoBoxCoords.style.display = 'flex';
   }
 
   /*
@@ -184,7 +201,6 @@ function App() {
     if (placeCode.length > 4){
       direction0PlaceCode = placeCode.substring(0,4);
     }
-    
     var circleElement = document.getElementById(direction0PlaceCode);
     var infoBoxCoords = document.getElementById('hide');
     
@@ -193,17 +209,41 @@ function App() {
 
     var infoBoxCenterLeftRight = (infoBoxRect.right - infoBoxRect.left);
     var infoBoxCenterUpDown = (infoBoxRect.bottom - infoBoxRect.top);
+    //console.log(screen.height);
 
-    infoBoxCoords.style.left = Math.max(((circleRect.right - (circleElement.getAttribute('r'))) - (infoBoxCenterLeftRight/2)),0) + 'px';
-    infoBoxCoords.style.top = Math.max(((circleRect.top) - infoBoxCenterUpDown),0) + 'px';
-
-    
+    if (screen.height > 900){
+      infoBoxCoords.style.left = Math.max(((circleRect.right - (circleElement.getAttribute('r'))) - (infoBoxCenterLeftRight/2)),0) + 'px';
+      infoBoxCoords.style.top = Math.max(((circleRect.top) - infoBoxCenterUpDown),0) + 'px';
+    }
+    else{
+      infoBoxCoords.style.bottom = 0 + 'px';
+    }
   }  
 
+  //Moves train to next station
+  async function trackTransport(directionCode){
+    for (let i = 0; i < currentLinesStops.length; i++){
+      
+      if (displayedPlaceCode == currentLinesStops[i].place_code){
+        
+        //gets data from one direction    
+        const nextStopFetch = await fetch('https://svc.metrotransit.org/nextrip/' + currentLineID + '/' + directionCode + '/' + currentLinesStops[i+1].place_code);
+        var nextStop = await nextStopFetch.json();        
+        
+        if (tracking == 0 && nextStop.departures[0].trip_id == displayedTripID){
+          tracking = 1;
+          //console.log(nextStop.departures[0].trip_id);
+          displayInfoBox(currentLineID, false, currentLinesStops[i+1].place_code, 0);
+          setPlaceCode(currentLinesStops[i+1].place_code);
+        }
+      }
+    }
+  }
+  
   return (
     <>
-      <MetroMap moveInfoBox={moveInfoBox} getDisplayedPlaceCode= {getDisplayedPlaceCode} currentLineID ={currentLineID} setPlaceCode={setPlaceCode} placeCode = {displayedPlaceCode} displayInfoBox ={displayInfoBox} stationsStops={stationLines} updateLinesAtStation={updateLinesAtStation}/>
-      <InfoBox changeLine = {changeLine} lineColors= {lineColors} placeCode = {displayedPlaceCode} stationsStops={stationLines} displayInfoBox ={displayInfoBox}/>
+      <MetroMap setCurrentLinesStops = {setCurrentLinesStops} moveInfoBox={moveInfoBox} getDisplayedPlaceCode= {getDisplayedPlaceCode} currentLineID ={currentLineID} setPlaceCode={setPlaceCode} placeCode = {displayedPlaceCode} displayInfoBox ={displayInfoBox} stationsStops={stationLines} updateLinesAtStation={updateLinesAtStation}/>
+      <InfoBox trackTransport = {trackTransport} changeLine = {changeLine} lineColors= {lineColors} placeCode = {displayedPlaceCode} stationsStops={stationLines} displayInfoBox ={displayInfoBox}/>
     </>
   )
 }
