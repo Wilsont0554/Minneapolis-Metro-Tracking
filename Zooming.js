a224efb24de01159a2018ba01538cac93eef3d80
Wilsont0554 <wilsont0554@my.uwstout.edu>
2025-02-20 20:16:07 -0600

    Update Zooming.js

--- Zooming.js
+++ Zooming.js
@@ -1,9 +1,19 @@
+  
   window.onload = function (){
+
+    var oldX = 0;
+    var oldY = 0;
+    var zoomAmount = 1.5;
+
+    var isMouseDown = false
+
     const navMap = {
       187: {act: 'zoom', dir: 1,  name: 'in'}, // ->
       189: {act: 'zoom', dir: -1, name: 'out'}, //
       61: {act: 'zoom', dir: 1,  name: 'in'}, // ->
       173: {act: 'zoom', dir: -1, name: 'out'}, //
+      1000:  {act: 'zoom', dir: 1, name: 'in'}, //scroll up
+      900:  {act: 'zoom', dir: -1, name: 'out'}, //scroll down
       39:  {act: 'move', dir: 1,  name: 'right',  axis: 0}, // ->
       40:  {act: 'move', dir: 1,  name: 'down',   axis: 1}, //
       37:  {act: 'move', dir: -1, name: 'left',   axis: 0}, //
@@ -11,11 +21,16 @@
     }, 
       _SVG = document.querySelector('svg'),
       VB = _SVG.getAttribute('viewBox').split(' ').map(c=>+c),
-      DMAX = VB.slice(2), WMIN = 480, NF = 12;
+      DMAX = VB.slice(2), WMIN = 960, NF = 1;
     
     let nav = null, tg = Array(4), f =0, rID = null;
-    
+
+    if (screen.height < 1200){
+      document.getElementById('svgWrapperWrapper').style.height = screen.height + 'px';
+    }
+
     function update(){
+
       let k = ++f/NF, j = 1-k, cvb = VB.slice();
 
       if (nav.act == 'zoom'){
@@ -43,39 +58,154 @@
       rID = requestAnimationFrame(update);
     }
 
-    document.addEventListener('keyup', e => {
-      if(e.keyCode in navMap){
-        nav = navMap[e.keyCode];
-        if (nav.act == 'zoom'){
-          if((nav.dir == -1 && VB[2] >= DMAX[0] * 2) || (nav.dir == 1 && VB[2] <= WMIN)){
-            //console.log(`cannot zoom ${nav.name} more`);
-            return;
+  document.addEventListener('mousemove', e => {  
+    if (isMouseDown){
+
+      //right
+      if (e.pageX < oldX) {
+        var moveAmount = oldX - e.pageX;
+        nav = navMap[39];
+
+        if (nav.dir == 1 && VB[nav.axis] <= DMAX[nav.axis]  * 2 - VB[nav.axis + 2]){
+          tg[0] = VB[0] + moveAmount * 1;
+          update();
+        }
+        else{
+          //tg[0] = DMAX[nav.axis] * 2 - VB[nav.axis + 2] - 500;
+          console.log("can't go right anymore");
+        }
+      } 
+      
+      //left
+      else if (e.pageX > oldX) {
+        var moveAmount = e.pageX - oldX;
+        nav = navMap[37];
+
+        if ((nav.dir == -1 && VB[nav.axis] > 0)){
+          tg[0] = VB[0] + moveAmount * -1;
+          update();
+        }
+        else{
+          //tg[0] = (DMAX[nav.axis] * 2 - VB[nav.axis + 2] - 500) * -1;
+          console.log("can't go left anymore");
+        }
+      }
+
+      //down
+      if (e.pageY < oldY) {
+        var moveAmount = oldY - e.pageY;
+        nav = navMap[40];
+
+        //console.log((VB[nav.axis]), Math.abs(DMAX[nav.axis] * 2 - VB[nav.axis + 2]));
+
+        if (nav.dir == 1 && VB[nav.axis] <= DMAX[nav.axis] * 1.5 - VB[nav.axis + 2]){
+          tg[1] = VB[1] + moveAmount * 1;
+          update();
+        }
+        else{
+          //tg[1] = (DMAX[nav.axis] - 500);
+          console.log("can't go down anymore");
+        }
+      } 
+      
+      //up
+      else if (e.pageY > oldY) {
+        var moveAmount = e.pageY - oldY;
+        nav = navMap[38];       
+       
+        if ((nav.dir == -1 && VB[nav.axis] > 0)) {
+            tg[1] = VB[1] + moveAmount * -1;
+            update();
           }
-        
-          for (let i = 0; i < 2; i++){
-            tg[i + 2] = VB[i + 2]/Math.pow(2, nav.dir);
-            tg[i] = 0.5 * (DMAX[i] - tg[i + 2]);
-          }
+          /*
+        if(((VB[nav.axis] * -1) <= (DMAX[nav.axis] * zoomAmount - VB[nav.axis + 2]))){ //!
+        }*/
+        else{
+          //tg[1] = DMAX[nav.axis] * -1 + 500;
+          console.log("can't go up anymore");
         }
 
-        //panning
-        else if (nav.act == 'move'){
+      }
+
+    }
+    oldY = e.pageY;
+    oldX = e.pageX;   
+  }, false);
+
+  document.addEventListener('mousedown', e => {
+    document.body.style.cursor = "grabbing";
+    isMouseDown = true;
+    oldY = e.pageY;
+    oldX = e.pageX;
+  }, false);
+
+  document.addEventListener('mouseup', e => {
+    document.body.style.cursor = "grab";
+
+    isMouseDown = false;
+  }, false);
+
+  if (screen.width > 1000){
+
+  document.addEventListener('wheel', e => {
+    //zoom in
+    const element = document.getElementById('newMap');
+
+    if(e.deltaY == -100){
+      nav = navMap[1000];
+      
+      if (nav.act == 'zoom'){
+        if((nav.dir == -1 && VB[2] >= DMAX[0] * 2) || (nav.dir == 1 && VB[2] <= WMIN)){
+          //console.log(`cannot zoom ${nav.name} more`);
+          return;
+        }
+        zoomAmount += 0.1;
+
+        for (let i = 0; i < 2; i++){
+          
+          tg[i + 2] = VB[i + 2]/Math.pow(1.1, nav.dir);
+          tg[i] = 0.5 * (DMAX[i] - tg[i+2]) ;
+        }
+      }
+    }
+
+    // zoom out
+    else if(e.deltaY == 100){
+      nav = navMap[900];
+      zoomAmount -= 0.1;
+
+      if (nav.act == 'zoom'){
+        if((nav.dir == -1 && VB[2] >= DMAX[0] * 2) || (nav.dir == 1 && VB[2] <= WMIN)){
+          //console.log(`cannot zoom ${nav.name} more`);
+          return;
+        }
+      
+        for (let i = 0; i < 2; i++){
+          tg[i + 2] = VB[i + 2]/Math.pow(1.1, nav.dir);
+          tg[i] = 0.5 * (DMAX[i] - tg[i+2]) ;
+        }
+      }
+    }
+   
+
+    tg[0] = VB[0];
+    tg[1] = VB[1];
+    update();
+
+  }, false);
+  }
+}
+
+/* OLD panning
+        if (nav.act == 'move'){
           //console.log(DMAX[nav.axis], VB[nav.axis + 2], VB[nav.axis]);
           if((nav.dir == -1 && VB[nav.axis] <= 0 ) || (nav.dir == 1 && VB[nav.axis] >= DMAX[nav.axis] * 2 - VB[nav.axis + 2])){
             //console.log(`cannot move ${nav.name} more`);
             return;
           }
-          tg[nav.axis] = VB[nav.axis] + 0.5 * nav.dir * VB[nav.axis + 2];
-          
-        }
-        
-
-        update();
-
-      }
-    }, false);
-  }
-
+          tg[nav.axis] = VB[nav.axis] + 0.25 * nav.dir * VB[nav.axis + 2];
+        }       
+        */
 
 /*var zoomAmount = 0.75;
 var panAmount = 0.75;
