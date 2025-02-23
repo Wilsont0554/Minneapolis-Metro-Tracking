333c4c60f9377c415c982241abc752de856574f2
Wilsont0554 <wilsont0554@my.uwstout.edu>
2025-02-23 10:58:17 -0600

    Update Zooming.js

--- Zooming.js
+++ Zooming.js
@@ -1,31 +1,7 @@
 
 
     window.onload = function(){
-        var touchScreen = false;
-        var oldX = 0;
-        var oldY = 0;
-        var numberOfTouches = 0;
-        var touchValues = [
-            [0,0],
-            [0,0]
-        ];
-
         
-        
-        var WMIN = 480;
-        var NF = 1;
-        let nav = null;
-        let tg = Array(4);
-        let f =0;
-        let rID = null;
-
-        function update(){
-
-            let k = ++f/NF;
-            let j = 1-k;
-      
-          }          
-
         document.addEventListener('touchstart', e => {
             document.getElementById('newMap').style.backgroundColor = ('rgb(120, 120, 120)');
             //nav = navMap[39];
