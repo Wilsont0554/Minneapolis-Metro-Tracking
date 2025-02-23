7113a9a9d259d5dfa74e7bfa3f8829f1626bdb40
Wilsont0554 <wilsont0554@my.uwstout.edu>
2025-02-23 13:02:27 -0600

    Update Zooming.js

--- Zooming.js
+++ Zooming.js
@@ -15,26 +15,32 @@ window.onload = function(){
         38:  {act: 'move', dir: -1, name: 'up',     axis: 1} //
       };
     var _SVG = document.getElementById('newMap');
-   
+    _SVG.setAttribute('viewBox', '0 0 900 1400');
+    var horizontalMovement = 0;
+
+    var WMIN = 480;
+    var NF = 1;
+    let nav = null;
+    let tg = Array(4);
+    let f =0;
+
 
     function update(){
 
         let k = ++f/NF;
         let j = 1-k;
-        let cvb = VB.slice();
   
         if (nav.act == 'zoom'){
           for (let i = 0; i < 4; i++) {
-            cvb[i] = j * VB[i] + k * tg[i];
-            
+            //cvb[i] = j * VB[i] + k * tg[i];
           }
         }
   
         else if (nav.act == 'move') {
-          cvb[nav.axis] = j * VB[nav.axis] + k * tg[nav.axis];
+          //cvb[nav.axis] = j * VB[nav.axis] + k * tg[nav.axis];
         }
   
-        _SVG.setAttribute('viewBox', cvb.join(' '));
+        //_SVG.setAttribute('viewBox', cvb.join(' '));
     } 
 
     document.addEventListener('touchstart', e => {
@@ -53,7 +59,8 @@ window.onload = function(){
     document.addEventListener('touchmove', e => {
         document.getElementById('newMap').style.backgroundColor = ('rgb(152, 223, 184)');
         //nav = navMap[39];
-        _SVG.setAttribute('viewBox', '0 0 940 1400');
+        horizontalMovement += 10;
+        _SVG.setAttribute('viewBox', ` ${horizontalMovement} 0 900 1400`);
         //tg[0] = VB[0] + 10;
         //update();
 
