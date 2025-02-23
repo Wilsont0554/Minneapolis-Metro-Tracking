3c78f1e32cee9a1eb555614c8b633c2041438751
Wilsont0554 <wilsont0554@my.uwstout.edu>
2025-02-23 11:16:40 -0600

    Update Zooming.js

--- Zooming.js
+++ Zooming.js
@@ -1,26 +1,25 @@
 
 
-    window.onload = function(){
+window.onload = function(){
 
-        var _SVG = document.querySelector('svg');
-        _SVG.setAttribute('viewBox', '0 0 900 1400');
-        
-        document.addEventListener('touchstart', e => {
-            document.getElementById('newMap').style.backgroundColor = ('rgb(120, 120, 120)');
-            //nav = navMap[39];
-            //tg[0] = VB[0] + 20;
-            //update();
-        }, false)
+    var _SVG = document.querySelector('svg');
+    
+    document.addEventListener('touchstart', e => {
+        document.getElementById('newMap').style.backgroundColor = ('rgb(120, 120, 120)');
+        //nav = navMap[39];
+        //tg[0] = VB[0] + 20;
+        //update();
+    }, false)
 
-        /*
-        document.addEventListener('touchend', e => {
-            numberOfTouches--;
-            touchScreen = false;
-        }, false)*/
+    /*
+    document.addEventListener('touchend', e => {
+        numberOfTouches--;
+        touchScreen = false;
+    }, false)*/
 
-        document.addEventListener('touchmove', e => {
-            document.getElementById('newMap').style.backgroundColor = ('rgb(217, 223, 152)');
+    document.addEventListener('touchmove', e => {
+        document.getElementById('newMap').style.backgroundColor = ('rgb(217, 223, 152)');
 
-        }, false)
-        
-    }
+    }, false)
+    
+}
