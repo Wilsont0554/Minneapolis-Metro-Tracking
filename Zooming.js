b2aa5c46024d7b73798fe9ff2c5c5f16516da814
Wilsont0554 <wilsont0554@my.uwstout.edu>
2025-02-23 10:17:59 -0600

    Update Zooming.js

--- Zooming.js
+++ Zooming.js
@@ -1,49 +1,11 @@
 
 
     window.onload = function(){
-        var touchScreen = false;
-        var oldX = 0;
-        var oldY = 0;
-        var numberOfTouches = 0;
-        var touchValues = [
-            [0,0],
-            [0,0]
-        ];
-
-        const navMap = {
-            187: {act: 'zoom', dir: 1,  name: 'in'}, // ->
-            189: {act: 'zoom', dir: -1, name: 'out'}, //
-            61: {act: 'zoom', dir: 1,  name: 'in'}, // ->
-            173: {act: 'zoom', dir: -1, name: 'out'}, //
-            1000:  {act: 'zoom', dir: 1, name: 'in'}, //scroll up
-            900:  {act: 'zoom', dir: -1, name: 'out'}, //scroll down
-            39:  {act: 'move', dir: 1,  name: 'right',  axis: 0}, // ->
-            40:  {act: 'move', dir: 1,  name: 'down',   axis: 1}, //
-            37:  {act: 'move', dir: -1, name: 'left',   axis: 0}, //
-            38:  {act: 'move', dir: -1, name: 'up',     axis: 1} //
-          }, 
-        _SVG = document.querySelector('svg');
-        _SVG.setAttribute('viewBox', '0 0 900 1400');
-        VB = _SVG.getAttribute('viewBox').split(' ').map(c=>+c),
-        DMAX = VB.slice(2), WMIN = 480, NF = 1;
-        let nav = null, tg = Array(4), f =0, rID = null;
-
-        function update(){
-
-            let k = ++f/NF, j = 1-k, cvb = VB.slice();
-            
-            if (nav.act == 'move') {
-              cvb[nav.axis] = j * VB[nav.axis] + k * tg[nav.axis];
-            }
-      
-            _SVG.setAttribute('viewBox', cvb.join(' '));
-    
-          }          
-
+        
         document.addEventListener('touchstart', e => {
             document.getElementById('newMap').style.backgroundColor = ('rgb(120, 120, 120)');
-            nav = navMap[39];
-            tg[0] = VB[0] + 20;
+            //nav = navMap[39];
+            //tg[0] = VB[0] + 20;
             //update();
         }, false)
 
@@ -51,103 +13,11 @@
         document.addEventListener('touchend', e => {
             numberOfTouches--;
             touchScreen = false;
-        }, false)
+        }, false)*/
 
         document.addEventListener('touchmove', e => {
-            //panning
-            if (touchScreen && numberOfTouches == 1){
-                //swipe right
-                if (e.touches[0].clientX < oldX) {
-                    var moveAmount = oldX - e.touches[0].clientX;
-                    nav = navMap[39];
-                    
-                    if (nav.dir == 1 && VB[nav.axis] <= DMAX[nav.axis]  * 2.5 - VB[nav.axis + 2]){
-                        tg[0] = VB[0] + moveAmount * 2;
-                        update();
-                    }
+            document.getElementById('newMap').style.backgroundColor = ('rgb(217, 223, 152)');
 
-                } 
-
-                //swipe left
-                else if (e.touches[0].clientX > oldX) {
-                    var moveAmount = e.touches[0].clientX - oldX;
-                    nav = navMap[37];
-            
-                    if ((nav.dir == -1 && VB[nav.axis] > 0)){
-                        tg[0] = VB[0] + moveAmount * -2;
-                        update();
-                    }
-                } 
-
-                //swipe down
-                if (e.touches[0].clientY < oldY) {
-                    var moveAmount = oldY - e.touches[0].clientY;
-                    nav = navMap[40];
-
-                    if (nav.dir == 1 && VB[nav.axis] <= DMAX[nav.axis] * 1.5 - VB[nav.axis + 2]){
-                        tg[1] = VB[1] + moveAmount * 2;
-                        update();
-                    }                    
-                } 
-
-                //up
-                else if (e.touches[0].clientY > oldY) {
-                    var moveAmount = e.touches[0].clientY - oldY;
-                    nav = navMap[38];       
-                
-                    if ((nav.dir == -1 && VB[nav.axis] > 0)) {
-                        tg[1] = VB[1] + moveAmount * -2;
-                        update();
-                    }
-                }
-                
-                oldY = e.touches[0].clientY;   
-                oldX = e.touches[0].clientX;
-            }
-            
-            //zooming
-            else if (numberOfTouches == 2){
-                var oldHypot = Math.hypot(touchValues[0][0] - touchValues[1][0], touchValues[0][1] - touchValues[1][1]);
-                var newHypot = Math.hypot(e.touches[0].clientX - e.touches[1].clientX, e.touches[0].clientY - e.touches[1].clientY);
-                
-                if (oldHypot > newHypot){
-                    nav = navMap[900];
-
-                    if((nav.dir == -1 && VB[2] >= DMAX[0] * 2) || (nav.dir == 1 && VB[2] <= WMIN)){
-                        return;
-                    }
-
-                    for (let i = 0; i < 2; i++){
-                        tg[i + 2] = VB[i + 2]/Math.pow(1.02, nav.dir);
-                        tg[i] = 0.1 * (DMAX[i] - tg[i+2]) ;
-                    }
-                    tg[0] = VB[0];
-                    tg[1] = VB[1];
-                    update(); 
-                }
-                else if (oldHypot < newHypot){
-                    nav = navMap[1000];
-
-                    if((nav.dir == -1 && VB[2] >= DMAX[0] * 2) || (nav.dir == 1 && VB[2] <= WMIN)){
-                        return;
-                    }
-
-                    for (let i = 0; i < 2; i++){
-                        tg[i + 2] = VB[i + 2]/Math.pow(1.02, nav.dir);
-                        tg[i] = 0.1 * (DMAX[i] - tg[i+2]) ;
-                    }
-                    tg[0] = VB[0];
-                    tg[1] = VB[1];
-                    update(); 
-                }
-                
-                touchValues[0][0] = e.touches[0].clientX;
-                touchValues[0][1] = e.touches[0].clientY;
-            
-                touchValues[1][0] = e.touches[1].clientX;
-                touchValues[1][1] = e.touches[1].clientY;
-            }
-            
         }, false)
-        */
+        
     }
